50fa8fd4b6512e786e7ce1d32e10e2a86d869563
Devin J. Pohly <djpohly@gmail.com>
2022-05-18 14:49:47 -0500

    add type signatures

--- gpsaml/impl.py
+++ gpsaml/impl.py
@@ -5,21 +5,22 @@ from fido2.client import Fido2Client
 from fido2.utils import websafe_decode, websafe_encode
 from fido2.webauthn import PublicKeyCredentialDescriptor as PKCD
 from fido2.webauthn import PublicKeyCredentialRequestOptions as PKCRO
+from typing import Any
 
 
 class Fido2Impl:
-    def __init__(self, origin):
+    def __init__(self, origin: str):
         dev = next(CtapHidDevice.list_devices(), None)
         assert(dev is not None)
 
         self.client = Fido2Client(dev, origin)
 
     @staticmethod
-    def device_present():
+    def device_present() -> bool:
         dev = next(CtapHidDevice.list_devices(), None)
         return dev is not None
 
-    def get_signature(self, appid, key: PKCD, clientData):
+    def get_signature(self, appid: str, key: PKCD, clientData: dict[str, Any]) -> tuple[str, str, str]:
         result = self.client.get_assertion(PKCRO(
             challenge=websafe_decode(clientData["challenge"]),
             rp_id=appid,
@@ -33,5 +34,5 @@ class Fido2Impl:
             websafe_encode(result.client_data),
         )
 
-    def key_for_id(self, keyid_b64: str):
+    def key_for_id(self, keyid_b64: str) -> PKCD:
         return PKCD(type="public-key", id=websafe_decode(keyid_b64))
