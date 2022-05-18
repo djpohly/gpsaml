6abbdaae62fbef359906e5603617162ca3097647
Devin J. Pohly <djpohly@gmail.com>
2022-05-18 14:52:42 -0500

    merge impl.py

--- gpsaml/__main__.py
+++ gpsaml/__main__.py
@@ -9,13 +9,47 @@
 
 import base64
 from bs4 import BeautifulSoup
+from fido2.hid import CtapHidDevice
+from fido2.client import Fido2Client
+from fido2.utils import websafe_decode, websafe_encode
+from fido2.webauthn import PublicKeyCredentialDescriptor as PKCD
+from fido2.webauthn import PublicKeyCredentialRequestOptions as PKCRO
 import json
 import requests
 import sys
+from typing import Any
 import urllib.parse
 import xml.etree.ElementTree as ET
 
-from .impl import Fido2Impl
+
+class Fido2Impl:
+    def __init__(self, origin: str):
+        dev = next(CtapHidDevice.list_devices(), None)
+        assert(dev is not None)
+
+        self.client = Fido2Client(dev, origin)
+
+    @staticmethod
+    def device_present() -> bool:
+        dev = next(CtapHidDevice.list_devices(), None)
+        return dev is not None
+
+    def get_signature(self, appid: str, key: PKCD, clientData: dict[str, Any]) -> tuple[str, str, str]:
+        result = self.client.get_assertion(PKCRO(
+            challenge=websafe_decode(clientData["challenge"]),
+            rp_id=appid,
+            allow_credentials=[key],
+            extensions=clientData["clientExtensions"],
+        )).get_response(0)
+
+        return (
+            websafe_encode(result.authenticator_data),
+            result.signature.hex(),
+            websafe_encode(result.client_data),
+        )
+
+    def key_for_id(self, keyid_b64: str) -> PKCD:
+        return PKCD(type="public-key", id=websafe_decode(keyid_b64))
 
 
 def main():
